5303d56991b4843b8c2ece06ee29c275d79c289f
Adari Harsha Vardhan Sandeep <106430230+Adari-Harsha@users.noreply.github.com>
2024-11-03 20:53:22 +0100

    Update Lax Freidrich Scheme.py

--- Lax Freidrich Scheme.py
+++ Lax Freidrich Scheme.py
@@ -21,52 +21,35 @@ gam = dt / h
 n = int(2 * L / h) + 1
 m = int(T / dt)
 
-# Initialize the spatial domain
 x = np.linspace(-L, L, n)
 u = u0(x)
 
-# Time evolution storage
 u_all_time_steps = []
 
-# Store the initial condition
 u_all_time_steps.append(u.copy())
 
-# Time loop
 for t in np.linspace(dt, T, m):
-    # Compute solution at new time level using central difference
     u[1:-1] = (u[2:] + u[:-2]) /  2 - c * gam * (u[2:] - u[:-2]) / 2
-
-    # Periodic boundary conditions
     u[0] = (u[1] + u[-1]) / 2 - c * gam * (u[1] - u[-1]) / 2
     u[-1] = (u[-1] + u[-2]) / 2 - c *  gam * (u[0] - u[-2]) / 2
-
-    # Store the solution at each time step
     u_all_time_steps.append(u.copy())
 
-# Create the figure and axis for the animation
 fig, ax = plt.subplots()
 ax.set(xlim=(-L, L), ylim=(-1, 2), xlabel='x', ylabel='u')
 ax.set_title('Lax Freidrich Scheme')
-
-# Initialize the line that will be updated during the animation
 line, = ax.plot([], [], 'b-')
-
-# Time annotation (counter)
 time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
 
-# Initialization function for the animation
 def init():
     line.set_data([], [])
     time_text.set_text('Time = 0.00')
     return line, time_text
 
-# Animation function, called for each frame
 def animate(i):
     line.set_data(x, u_all_time_steps[i])
-    time_text.set_text(f'Time = {i * dt:.3f} s')  # Update the time counter
+    time_text.set_text(f'Time = {i * dt:.3f} s')  
     return line, time_text
 
-# Create the animation object
 anim = FuncAnimation(fig, animate, init_func=init, frames=len(u_all_time_steps),
                      interval=50, blit=True)
 
